8549f5acf189000c47923454a76e8c2da12f3a60
dairagalceran <dairagalceran@gmail.com>
2024-06-06 22:53:07 -0300

    agrega interfaz del template del componente lista-general

---
+++ src/app/listado-general/Categoria.ts
@@ -0,0 +1,9 @@
+export interface Categoria {
+  name: string,
+  price: number,
+  stock: number,
+  image: string,
+  clearence: boolean,
+}
+
+//export permite que la clase pueda ser importada por otras clases
--- src/app/listado-general/listado-general.component.ts
+++ src/app/listado-general/listado-general.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Categoria } from './Categoria';
+
 
 @Component({
   selector: 'app-listado-general',
@@ -7,11 +9,29 @@ import { Component } from '@angular/core';
 })
 export class ListadoGeneralComponent {
 
-  categoria = {
+  //MOCK utilizado para probar el front end hasta que los datos se traigan de una API
+  categorias: Categoria[] = [
+    {
     "name": "Bolsa reutilizable",
     "price": 4000,
     "stock": 3,
-    "image": "assets/img/bolsa.png"
+    "image": 'assets/img/bolsa.png',
+    "clearence": true,
+  },
+  {
+    "name": "Mochilas",
+    "price": 7500,
+    "stock": 0,
+    "image": 'assets/img/mochila.webp',
+    "clearence": false,
+  },
+  {
+    "name": "Riñoneras",
+    "price": 5500,
+    "stock": 12,
+    "image": 'assets/img/rinonera.png',
+    "clearence": false,
   }
+];
 
 }
